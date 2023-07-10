b88b9e1a669cfcb05eb38174b05e37033abea73c
alekelbar <alekelbar.personal@gmail.com>
2023-07-10 01:51:30 -0600

    chore: updating theme

--- lib/config/theme/theme_config.dart
+++ lib/config/theme/theme_config.dart
@@ -4,6 +4,7 @@ class ThemeConfig {
   final ThemeData theme = ThemeData(
     colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
     useMaterial3: true,
+    // brightness: Brightness.dark,
   );
 
   ThemeData getTheme() => theme;
