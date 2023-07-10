21a6c50d7414535a5870ce6d7fc99cf559bb9ceb
alekelbar <alekelbar.personal@gmail.com>
2023-07-10 01:51:51 -0600

    feat: bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gym_controller/config/router/app_router.dart';
 import 'package:gym_controller/config/theme/theme_config.dart';
 import 'package:gym_controller/firebase_options.dart';
+import 'package:gym_controller/infrastructure/datasources/firebase_auth_datasource_impl.dart';
+import 'package:gym_controller/infrastructure/repositories/firebase_auth_repository_impl.dart';
+import 'package:gym_controller/presentation/bloc/auth/auth_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,7 +14,15 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+
+  final repository = FirebaseAuthRepositoryImpl(
+    datasource: FirebaseAuthDatasourceImpl(),
+  );
+
+  runApp(BlocProvider(
+    create: (context) => AuthBloc(repository: repository),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
