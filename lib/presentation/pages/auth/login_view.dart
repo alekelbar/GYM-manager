8011a0c2b3151c58af24109b2cef6168668e45b5
alekelbar <alekelbar.personal@gmail.com>
2023-07-10 21:24:00 -0600

    chore: remove unused imports

--- lib/presentation/pages/auth/login_view.dart
+++ lib/presentation/pages/auth/login_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:gym_controller/presentation/bloc/auth/auth_bloc.dart';
 import 'package:gym_controller/presentation/pages/home/home_view.dart';
 
